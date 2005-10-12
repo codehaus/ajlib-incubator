07562c1e602207e47a65c470558321327904b3cd
aschmidmeier <aschmidmeier@88504d66-b372-0410-9f79-a629e3f8573f>
2005-10-12 21:23:31 +0000

    A common testcase class

---
+++ org.codehaus.ajlib/testsrc/org/codehaus/ajlib/AllSETests.java
@@ -0,0 +1,24 @@
+package org.codehaus.ajlib;
+
+import org.codehaus.ajlib.util.ressourcecontroller.RessourceControllerTestCase;
+import org.codehaus.ajlib.util.version.VersionTester;
+import org.codehaus.ajlib.util.tracing.ExecutionTracingTestCase;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+public class AllSETests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for org.codehaus.ajlib");
+		//$JUnit-BEGIN$
+
+		//$JUnit-END$
+		suite.addTestSuite(VersionTester.class);
+		suite.addTestSuite(RessourceControllerTestCase.class);
+        suite.addTestSuite(ExecutionTracingTestCase.class);
+		return suite;
+	}
+
+}
