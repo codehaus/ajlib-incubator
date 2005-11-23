cad7502acf750b60588ac0c47216562cdcd50213
ebodden <ebodden@88504d66-b372-0410-9f79-a629e3f8573f>
2005-11-23 17:57:01 +0000

    *** empty log message ***

--- org.codehaus.ajlib5/src/org/codehaus/ajlib/util/AutoEquals.java
+++ org.codehaus.ajlib5/src/org/codehaus/ajlib/util/AutoEquals.java
@@ -8,7 +8,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * 
+ * Marker annotation for Fields and Classes.
+ * @see AutoEqualsAspect
  * 
  * @author Eric Bodden
  */
