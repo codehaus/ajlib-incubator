c6e6d0841075ac60a1abd3c8d19af393befd25e8
aschmidmeier <aschmidmeier@88504d66-b372-0410-9f79-a629e3f8573f>
2006-12-06 22:46:48 +0000

    Missing File from Ron, Referencedin PerThreadTracer

---
+++ org.codehaus.ajlib/src/org/codehaus/ajlib/error/AspectConfigurationException.java
@@ -0,0 +1,11 @@
+package org.codehaus.ajlib.error;
+
+public class AspectConfigurationException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+    
+    public AspectConfigurationException(String msg){
+        super(msg);
+    }
+    
+}
